2fb9285b6f2c7ff0fb97e643d8731038bfde9bce
nurasylkaiyr <kair.nurasyl@gmail.com>
2023-04-18 20:26:22 +0600

    MyArrayList implements MyList interface

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -1,2 +1,95 @@
-public class MyArrayList {
+public class MyArrayList<T> implements MyList<T>{
+    private T[] arr;
+    private int size;
+
+    MyArrayList(){
+        this.arr = (T[]) new Object[5];
+        this.size = 0;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    public void  add(T element){
+        if(size == arr.length){
+            increaseBuffer();
+        }
+        arr[size++] = element;
+    }
+
+    @Override
+    public void add(T item, int index) {
+
+    }
+
+    @Override
+    public boolean remove(T item) {
+        return false;
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    public void increaseBuffer(){
+        T[] newArr = (T[]) new Object[arr.length*2];
+        for(int i=0; i< arr.length; i++){
+            newArr[i]=arr[i];
+        }
+        arr = newArr;
+    }
+
+    public T getElement(int index) {
+        checkIndex(index);
+        return arr[index];
+    }
+
+    public int getSize() {
+        return size;
+    }
+    public void clear(){
+        this.arr = (T[]) new Object[5];
+        this.size = 0;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    public void delete(int index){
+        checkIndex(index);
+        for(int i= index + 1; i<size; i++){
+            arr[i-1] = arr[i];
+        }
+        size--;
+    }
+    public void checkIndex(int index){
+        if(index < 0 || index>=size){
+            throw new IndexOutOfBoundsException();
+        }
+    }
 }
