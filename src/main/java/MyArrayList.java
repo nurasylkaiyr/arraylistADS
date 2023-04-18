6ca529abe4e96c5cf2dff0428ff3603d7d50b26e
nurasylkaiyr <kair.nurasyl@gmail.com>
2023-04-18 20:31:32 +0600

    checking object o

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -14,6 +14,11 @@ public class MyArrayList<T> implements MyList<T>{
 
     @Override
     public boolean contains(Object o) {
+        for(int i = 0; i < size; i++){
+            if(arr[i].equals(o)){
+                return true;
+            }
+        }
         return false;
     }
 
