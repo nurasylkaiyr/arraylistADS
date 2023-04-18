66f709c9b1698caddc74daa74530d70e1b5b86da
nurasylkaiyr <kair.nurasyl@gmail.com>
2023-04-19 00:42:29 +0600

    class Node

--- src/main/java/org/example/MyLinkedList.java
+++ src/main/java/org/example/MyLinkedList.java
@@ -1,5 +1,68 @@
 package org.example;
 
-public class MyLinkedList {
+public class MyLinkedList<T> implements MyList<T> {
+    private class Node{
+        private T element;
+        private Node next;
+        private Node prev;
+        public Node(T element) {
+            this.element = element;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    @Override
+    public int size() {
+        return 0;
+    }
 
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public void add(T item) {
+
+    }
+
+    @Override
+    public void add(T item, int index) {
+
+    }
+
+    @Override
+    public boolean remove(T item) {
+        return false;
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
 }
