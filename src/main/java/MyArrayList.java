9e3854d8636c68491decd2d43c04c88544c9aab9
nurasylkaiyr <kair.nurasyl@gmail.com>
2023-04-18 23:03:25 +0600

    sort method

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -105,12 +105,30 @@ public class MyArrayList<T> implements MyList<T>{
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        for(int i = size - 1; i>=0; i--){
+            if(arr[i].equals(o)){
+                return i;
+            }
+        }
+        return -1;
+
     }
 
     @Override
     public void sort() {
-
+        boolean swap = true;
+        while (swap) {
+            swap = false;
+            for (int i = 1; i < size; i++) {
+                if (((Comparable) arr[i-1]).compareTo(arr[i]) > 0) {
+                    Object temp = arr[i-1];
+                    arr[i-1] = arr[i];
+                    arr[i] = (T) temp;
+                    swap = true;
+                }
+            }
+            size--;
+        }
     }
 
     public void delete(int index){
