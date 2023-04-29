9cb9c8efa81a5ce29e9cb470dbfa975730f953a0
nurasylkaiyr <kair.nurasyl@gmail.com>
2023-04-29 21:18:02 +0600

    Creating a constructor for the class and initializing the instance variable.

--- src/main/java/org/example/MyLinkedListStack.java
+++ src/main/java/org/example/MyLinkedListStack.java
@@ -2,6 +2,8 @@ package org.example;
 import java.util.EmptyStackException;
 public class MyLinkedListStack<T> {
     private MyLinkedList<T> list;
-
+    public MyLinkedListStack(){
+        list = new MyLinkedList<>();
+    }
 
 }
