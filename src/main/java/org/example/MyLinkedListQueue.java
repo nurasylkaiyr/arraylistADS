7664c7ae3f382c06b7d54f2a27a2e72d87f7b885
nurasylkaiyr <kair.nurasyl@gmail.com>
2023-04-29 21:51:08 +0600

    Declaring an instance variable of type MyLinkedList

--- src/main/java/org/example/MyLinkedListQueue.java
+++ src/main/java/org/example/MyLinkedListQueue.java
@@ -1,5 +1,7 @@
 package org.example;
 import  java.util.NoSuchElementException;
 import java.util.LinkedList;
-public class MyLinkedListQueue {
+public class MyLinkedListQueue<T> {
+    private LinkedList<T> list;
+
 }
