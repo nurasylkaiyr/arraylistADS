778cbee47ef21d4f01cc408480ff2312cc7d067e
nurasylkaiyr <kair.nurasyl@gmail.com>
2023-04-29 19:01:58 +0600

    constructor for the class and initialize the instance variable

--- src/main/java/org/example/MyArrayListStack.java
+++ src/main/java/org/example/MyArrayListStack.java
@@ -2,5 +2,7 @@ package org.example;
 import java.util.EmptyStackException;
 public class MyArrayListStack<T> {
     MyArrayList<T> list;
-
+    public MyArrayListStack(){
+        list = new MyArrayList<>();
+    }
 }
